aeceac7e8d013ee918b688a61da33ae9457a00fa
mendolawong <jwong.ncsu@gmail.com>
2019-11-09 16:51:47 -0500

    pre deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$6iHQB0wp8Eyr21qkNmfNNufWzmlhhdFiNPnFvoyhmHHpS0zydbYey',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
